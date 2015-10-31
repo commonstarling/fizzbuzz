4c97e19631088f52be850070c5d53b2ed0f1cd7b
commonstarling <sheenaramirez2@gmail.com>
2015-10-31 12:08:06 -0500

    finished product

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	for (var i = 1; i <= 100; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			document.write("FizzBuzz<br>");
+		}
+		else if (i % 3 === 0 && i % 5 !== 0) {
+			document.write("Fizz<br>");
+		}
+		else if (i % 5 === 0 && i % 3 !== 0) {
+			document.write("Buzz<br>");
+		}
+		else {
+			document.write(i + "<br>");
+		}
+	};
+});
